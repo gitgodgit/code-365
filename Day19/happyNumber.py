7e85f6ab226161caa4e8ed06adacc8478677dd3b
Noe Minidlaia <134667441+gitgodgit@users.noreply.github.com>
2025-01-20 00:11:15 +0400

    chao

--- Day19/happyNumber.py
+++ Day19/happyNumber.py
@@ -1,2 +1,13 @@
-def happyNumber():
-    pass
+def isHappy(n: int) -> bool:
+    string_n = str(n)
+    squares_sum = 0
+    for number in string_n:
+        squared = int(number) * int(number)
+        squares_sum += squared
+    if squares_sum == 1 or squares_sum == 7:
+        return True
+    elif squares_sum < 10:
+        return False
+    else:
+        return isHappy(squares_sum)
+    
