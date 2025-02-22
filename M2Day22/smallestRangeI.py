256b4df9747c9dbfd6faaedc9dc8656e50c53126
Noe Minidlaia <134667441+gitgodgit@users.noreply.github.com>
2025-02-22 14:58:54 +0400

    optimised

---
+++ M2Day22/smallestRangeI.py
@@ -0,0 +1,39 @@
+def smallestRangeI(nums: list[int], k: int) -> int:
+    maximum = max(nums)
+    minimum = min(nums)
+    average = round((maximum + minimum) / 2)
+    if maximum - minimum >= 2 * k:
+        for i in range(len(nums)):
+            if nums[i] == maximum:
+                nums[i] -= k
+            elif nums[i] == minimum:
+                nums[i] += k
+            elif nums[i] < average:
+                nums[i] += min(average - nums[i], k)
+            elif nums[i] > average:
+                nums[i] -= min(nums[i] - average, k)
+    else:
+        for i in range(len(nums)):
+            if nums[i] == maximum:
+                nums[i] -= maximum - average
+            elif nums[i] == minimum:
+                nums[i] += average - minimum
+            elif nums[i] < average:
+                nums[i] += min(average - nums[i], k)
+            elif nums[i] > average:
+                nums[i] -= min(nums[i] - average, k)
+    
+    return max(nums) - min(nums)
+
+def optimiseddefSmallestRangeI(nums: l[int], k: int) -> int:
+        maximum = max(nums)
+        minimum = min(nums)
+        average = round((maximum + minimum) / 2)
+        for i in range(len(nums)):
+            if nums[i] < average:
+                nums[i] += min(average - nums[i], k)
+            elif nums[i] > average:
+                nums[i] -= min(nums[i] - average, k)
+    
+        
+        return max(nums) - min(nums)
