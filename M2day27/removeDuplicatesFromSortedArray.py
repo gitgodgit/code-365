f31e993b56db87e82ffe3a705d3520ebc19edc25
Noe Minidlaia <134667441+gitgodgit@users.noreply.github.com>
2025-02-27 23:52:57 +0400

    woke

--- M2day27/removeDuplicatesFromSortedArray.py
+++ M2day27/removeDuplicatesFromSortedArray.py
@@ -1,2 +1,9 @@
 def removeDuplicates(nums: list[int]) -> int:
-    pass
+    if not nums:
+        return 0
+    k = 1
+    for i in range(1, len(nums)):
+        if nums[i] != nums[k -1]:
+            nums[k] = nums[i]
+            k+=1
+    return k
